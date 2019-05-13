e2e6b8c509c38df5d0beb0dc04eca6e66ef38c60
Keshav <keshav@Keshavs-MacBook-Pro.local>
2019-05-13 16:17:21 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { FeatureComponent } from './feature/feature.component';
+import { SkillsComponent } from './skills/skills.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { PlateformComponent } from './plateform/plateform.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    FeatureComponent,
+    SkillsComponent,
+    BlogsComponent,
+    PlateformComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/plateform/plateform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plateform',
+  templateUrl: './plateform.component.html',
+  styleUrls: ['./plateform.component.css']
+})
+export class PlateformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
